3ba1ded93159427b10d2e45e735db2207120ca75
Kent Yunge <yunges@Kents-iMac.local>
2017-01-22 11:27:28 -0500

    adding settings

---
+++ Settings.js
@@ -0,0 +1,9 @@
+const Settings = {
+    lightUrl: 'path/to/hue/lights',
+    google: {
+        projectId: 'google-project-id',
+        keyFilename: 'path/to/google/keystore.json'
+    }
+};
+
+module.exports = Settings;
